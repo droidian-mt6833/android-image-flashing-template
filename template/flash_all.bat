e4c667e3e550817851b7eaf4a9085aec649b0535
Bardia Moshiri <fakeshell@bardia.tech>
2023-02-06 19:04:11 -0500

    add vendor.img for lavender

--- template/flash_all.bat
+++ template/flash_all.bat
@@ -1,5 +1,6 @@
 fastboot flash boot data/boot.img
 fastboot flash dtbo data/dtbo.img
+fastboot flash vendor data/vendor.img
 fastboot flash vbmeta data/vbmeta.img
 fastboot flash userdata data/userdata.img
 
